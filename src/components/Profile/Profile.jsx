0295d8ccd3b1cb5394b7b6caf6e9017747e43b32
nicksolony <nicksolony@gmail.com>
2023-09-08 19:06:28 -0400

    finished css styling of Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,6 @@ import css from './Profile.module.css';
 
 export const Profile = ({user}) => {
 
-    console.log(user.username);
     return (
         <div className={css.profile}>
             <div className={css.description}>
@@ -18,18 +17,18 @@ export const Profile = ({user}) => {
             <p className={css.location}>{user.location}</p>
             </div>
 
-            <ul className="stats">
+            <ul className={css.stats}>
                 <li>
-                  <span className="label">Followers</span>
-                  <span className="quantity">{user.stats.followers}</span>
+                  <span className={css.label}>Followers</span>
+                  <span className={css.quantity}>{user.stats.followers}</span>
                 </li>
                 <li>
-                  <span className="label">Views</span>
-                  <span className="quantity">{user.stats.views}</span>
+                  <span className={css.label}>Views</span>
+                  <span className={css.quantity}>{user.stats.views}</span>
                 </li>
                 <li>
-                  <span className="label">Likes</span>
-                  <span className="quantity">{user.stats.likes}</span>
+                  <span className={css.label}>Likes</span>
+                  <span className={css.quantity}>{user.stats.likes}</span>
                 </li>
             </ul>
         </div>
