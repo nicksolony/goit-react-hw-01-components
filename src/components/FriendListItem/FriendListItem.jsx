4d396e1e814125308522169022708449277875c0
nicksolony <nicksolony@gmail.com>
2023-09-09 18:07:36 -0400

    added Name and Avatar styled components

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,12 @@
-import { Item, Status } from "./FriendListItem.styled";
+import { Item, Status, Avatar, Name } from "./FriendListItem.styled";
 
 
 export const FriendListItem = ({avatar, name, isOnline, id}) => {
     return (
         <Item key={id}>
              <Status status={isOnline}></Status>
-             <img class="avatar" src={avatar} alt="User avatar" width="48" />
-             <p class="name">{name}</p>
+             <Avatar src={avatar} alt="User avatar"/>
+             <Name>{name}</Name>
         </Item>
     )
 };
--- src/components/FriendListItem/FriendListItem.styled.jsx
+++ src/components/FriendListItem/FriendListItem.styled.jsx
@@ -8,6 +8,8 @@ export const Item = styled.li `
     width:100%;
     padding-top: 5px;
     padding-bottom: 5px;
+    border: 1px solid #818589;
+    box-shadow: 3px 3px 3px 0px #888888;
 `;
 
 export const Status = styled.span `
@@ -17,4 +19,15 @@ background-color: ${({ status }) => {
     border-radius: 50%; 
     width: 20px;
     height: 20px;
+`;
+
+export const Name = styled.p `
+    font-size: 18px;
+`;
+
+
+export const Avatar = styled.img `
+  width: 48px;
+  height: 48px;
+  border-radius: 3px;
 `;
