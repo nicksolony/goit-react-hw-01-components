38b060e33fb7b0713b702399eb99cfe3fab05ba1
nicksolony <nicksolony@gmail.com>
2023-09-08 22:42:04 -0400

    added Statistics.module.css

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import { StatisticsTitle } from 'components/StatisticsTitle/StatisticsTitle';
 import { StatisticsDataList } from 'components/StatisticsDataList/StatisticsDataList';
-// import css from './Statistics.module.css';
+import css from './Statistics.module.css';
 
 export const Statistics = ({title, stats}) => {
     return (
-        <section class="statistics">
+        <section className={css.statistics}>
           {<StatisticsTitle title={title}/>}
           {<StatisticsDataList stats={stats}/>}
         </section>
@@ -16,6 +16,7 @@ Statistics.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.exact({
+      id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
     })
