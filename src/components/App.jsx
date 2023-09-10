fdbd8842ff835b415560fbc1bd20ffcf2e709c88
nicksolony <nicksolony@gmail.com>
2023-09-09 22:12:04 -0400

    added TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 import user from '../data/user.json';
 import stats from '../data/statistics.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 
 export const App = () => {
@@ -24,7 +26,7 @@ export const App = () => {
       <Statistics stats={stats}/>
       <Statistics title="Upload stats" stats={stats} />
       <FriendList friends = {friends}/>
-
+      <TransactionHistory items = {transactions}/>
     </div>
   );
 };
